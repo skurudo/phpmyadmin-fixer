d63ac0b253bfdd7e042bb9d146c5face4a114013
Luiz Jr <luizjrdeveloper@gmail.com>
2019-04-06 00:30:47 -0300

    Att.

--- README.md
+++ README.md
@@ -1,76 +1,49 @@
-# phpmyadmin-fixer
-Fixes for phpmyadmin (configuration storage and some extended features)
+# phpMyAdmin-fixer
+Correções para o phpmyadmin (armazenamento de configuração e alguns recursos estendidos)
 
-If you're tired of the message "The phpMyAdmin configuration storage is not completely configured, some extended features have been deactivated", this patch for you ;-)
+Se você está cansado da mensagem "O armazenamento da configuração do phpMyAdmin não está completamente configurado, alguns recursos estendidos foram desativados", este patch para você ;-)
 
-##What do you get?
-- Tracking pages (version control MySQL table structure and data);
-- Bookmarks allow you to save predefined queries and execute them later;
-- You can show recently used tables in the navigation panel. It helps you jump across the table directly, without the need to select the database and the table;
-- Triggers - code which is automatically executed in response to certain events.
-- Designer mode (visually display your tables and a graphical way of creating, editing, and displaying phpMyAdmin relations);
-- Relation support (allows relationships (similar to foreign keys) using MySQL-native (InnoDB) methods);
-- PDF schema (phpMyAdmin can produce PDF schema of your database);
-- You can store comments to describe each column for each table. These will then be shown on the “printview”;
-- phpMyAdmin can be configured to remember several things, column order, and column visibility from a database table) for browsing tables;
-- Since release 3.4.x phpMyAdmin allows users to set most preferences by themselves and store them in the database;
-- Since release 4.2.0 you can save and load query-by-example searches from the Database > Query panel;
-- Since release 4.5.0 you can save and load export templates;
-- no message about "storage is not completely configured";
+## O que você ganha?
+* Páginas de rastreamento (estrutura de tabela e dados de controle de versão do MySQL);
+* Você pode mostrar tabelas usadas recentemente no painel de navegação. Ele ajuda você a pular a tabela diretamente, sem a necessidade de selecionar o banco de dados e a tabela;
+* Triggers - código que é executado automaticamente em resposta a certos eventos.
+* Modo Designer (exibe visualmente suas tabelas e uma maneira gráfica de criar, editar e exibir relações phpMyAdmin);
+* Suporte à relação (permite relacionamentos (similares a chaves estrangeiras) usando métodos nativos do MySQL (InnoDB);
+* Esquema PDF (o phpMyAdmin pode produzir o esquema PDF do seu banco de dados);
+* Você pode armazenar comentários para descrever cada coluna para cada tabela. Estes serão então mostrados no “printview”;
+* O phpMyAdmin pode ser configurado para lembrar várias coisas, a ordem das colunas e a visibilidade da coluna de uma tabela de banco de dados) para tabelas de navegação;
+* Desde o release 3.4.x, o phpMyAdmin permite que os usuários definam a maioria das preferências e armazenem-nas no banco de dados;
+* Since release 4.2.0 you can save and load query-by-example searches from the Database > Query panel;
+* Desde a versão 4.5.0, você pode salvar e carregar modelos de exportação;
+* Nenhuma mensagem sobre "armazenamento não está completamente configurado";
 
-_(information source - phpMyAdmin FAQ page)_
+_(fonte da informação: página do FAQ do phpMyAdmin)
 
-##Installation:
-__Universal installer with OS Checker__
+## Installation: __Instalador universal com o OS Checker__
 
-` curl -O -k https://raw.githubusercontent.com/skurudo/phpmyadmin-fixer/master/pma.sh && chmod +x pma.sh && ./pma.sh `
+```sh
+bash <(curl -s https://raw.github.com/luizjr/phpMyAdmin-Fixer-VestaCP/master/pma.sh)
+```
 
-or
-
-` wget --no-check-certificate https://raw.githubusercontent.com/skurudo/phpmyadmin-fixer/master/pma.sh && chmod +x pma.sh && ./pma.sh `
-
-##OS Installation:
-__Ubuntu__
-
-` sudo curl -O -k https://raw.githubusercontent.com/skurudo/phpmyadmin-fixer/master/pma-ubuntu.sh && sudo chmod +x pma-ubuntu.sh && sudo ./pma-ubuntu.sh `
-
-or
-
-` sudo wget --no-check-certificate https://raw.githubusercontent.com/skurudo/phpmyadmin-fixer/master/pma-ubuntu.sh && sudo chmod +x pma-ubuntu.sh && sudo ./pma-ubuntu.sh `
-
-__Debian__
-
-` curl -O -k https://raw.githubusercontent.com/skurudo/phpmyadmin-fixer/master/pma-debian.sh && chmod +x pma-debian.sh && ./pma-debian.sh `
-
-or		
-
-` wget --no-check-certificate https://raw.githubusercontent.com/skurudo/phpmyadmin-fixer/master/pma-debian.sh && chmod +x pma-debian.sh && ./pma-debian.sh `
-
-__CentOS__
-
-` curl -O -k https://raw.githubusercontent.com/skurudo/phpmyadmin-fixer/master/pma-centos.sh && chmod +x pma-centos.sh && ./pma-centos.sh `
-
-or
-
-` wget --no-check-certificate https://raw.githubusercontent.com/skurudo/phpmyadmin-fixer/master/pma-centos.sh && chmod +x pma-centos.sh && ./pma-centos.sh `
-
-##What the script does:
-- works from user root;
-- changes inside /etc/phpmyadmin/config.inc.php, fixes value;
-- makes a backup of /etc/phpmyadmin/config.inc.php in /root folder;
+## O que o script faz:
+- Funciona a partir do usuário root;
+- Muda dentro de /etc/phpmyadmin/config.inc.php, corrigindo os valores;
+- Faz um backup de /etc/phpmyadmin/config.inc.php na pasta /root;
 - adds in a mysql pma user / and the table phpmyadmin (if the user "pma" or the table "phpmyadmin" already exists, the script will delete those!);
 - download and add table for database phpmyadmin;
 - clean temp files, leftover pma.txt (contains password for pma@localhost) and old config;
 
-##Changes:
+## Alterações:
 - new tables and settings for favorite, usergroups, central_columns, designer_settings, export_templates;
 - we don't use pwgen for generation password anymore, no extra utilities;
 - check wget/curl before downloading dump for database;
 - universal installer with os detector by Sergey Rodin (VestaCP - https://vestacp.com);
 - options savedsearches / navigationhiding / users / usergroups are set (for 4.x phpmyadmin);
-- added table pma__usergroups (for 4.x phpmyadmin);
-- sql dump on github now;
-- sh files on github too.
+- Tabela pma__usergroups adicionada (para phpmyadmin 4.x);
+- SQL dump no github agora;
+- Scripts no github.
 
+#### Testado em servidores diferentes: Debian 7/8, Ubuntu 12/14/16.04/18.04, CentOS 6/7
+#### Suporte 3.x e versão 4.x do phpmyadmin
 ####Tested on different servers: Debian 7/8, Ubuntu 12/14/15, CentOS 6/7
 ####Support 3.x and 4.x version of phpmyadmin
--- pma.sh
+++ pma.sh
@@ -1,60 +1,397 @@
 #!/bin/bash
-# 
-# phpmyadmin-fixer
 #
-# Fixes for phpmyadmin (configuration storage and some extended features)
+# https://github.com/luizjr/phpMyAdmin-Fixer-VestaCP
+# Autor: Luiz Jr (https://luizjr.dev)
 #
-# If you're tired from message - "The phpMyAdmin configuration storage 
-# is not completely configured, some extended features have been deactivated", 
-# this patch for you ;-)
-#
-#
+# Based in
+# by Author - Pavel Galkin (https://skurudo.ru)
 # https://github.com/skurudo/phpmyadmin-fixer
-# Author - Pavel Galkin (https://skurudo.ru)
 #
-# Code for OS check / admin check / check curl/wget 
+# Code for check curl/wget
 # by Serghey Rodin (https://vestacp.com) from (https://github.com/serghey-rodin/vesta/)
 #
 # ...
 #
-# root check
-if [ "x$(id -u)" != 'x0' ]; then
-    echo 'Error: this script can only be executed by root'
-    exit 1
-fi
 
-# Detect OS
-case $(head -n1 /etc/issue | cut -f 1 -d ' ') in
-    Debian)     type="debian" ;;
-    Ubuntu)     type="ubuntu" ;;
-    CentOS)     type="centos" ;;
-    *)          type="centos" ;;	
-esac
+am_i_root(){
+	# Am I root?
+	if [ "x$(id -u)" != 'x0' ]; then
+		echo 'Erro: este script só pode ser executado pelo usuário root'
+		exit 1
+	fi
+}
 
-# Check wget
-if [ -e '/usr/bin/wget' ]; then
-    wget --no-check-certificate https://raw.githubusercontent.com/skurudo/phpmyadmin-fixer/master/pma-$type.sh -O pma-$type.sh
-    if [ "$?" -eq '0' ]; then
-        bash pma-$type.sh $*
-        exit
-    else
-        echo "Error: pma-$type.sh download via wget failed."
-        exit 1
-    fi
-fi
+detecta_sistema(){
+	# Detect OS
+	case $(head -n1 /etc/issue | cut -f 1 -d ' ') in
+		Debian)     type="debian" ;;
+		Ubuntu)     type="ubuntu" ;;
+		CentOS)     type="centos" ;;
+		*)          type="centos" ;;
+	esac
+}
 
-# Check curl
-if [ -e '/usr/bin/curl' ]; then
-    curl -O -k https://raw.githubusercontent.com/skurudo/phpmyadmin-fixer/master/pma-$type.sh
-    if [ "$?" -eq '0' ]; then
-        bash pma-$type.sh $*
-        exit
-    else
-        echo "Error: pma-$type.sh download via curl failed."
-        exit 1
-    fi
-fi
+boas_vindas(){
+	echo "phpMyAdmin Fixer: Corrige a configuração de armazenamento e de alguns recursos estendidos";
+	echo "................";
+	echo "Vamos fazer isso";
+	echo "............";
+	echo "..........";
+	echo ".........";
+	echo "........";
+	echo "......";
+}
 
-exit
+gera_senha_randomica(){
+	echo "Gere uma senha para o PMA (pma.txt)";
+	PASS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -c 32 ; echo`
+	echo "Senha do PMA = $PASS";
+}
 
+debian_phpmyadmin_path(){
+	#debian and ubuntu phpmyadmin path
+	pmapath1="/etc/phpmyadmin/config.inc.php"
+	pmapath2="/usr/share/phpmyadmin/config.sample.inc.php"
+	cp $pmapath1 /root/config.inc.php.old
+	cp $pmapath2 /root/config.inc.php.old2
+}
 
+centos_phpmyadmin_path(){
+	#centos phpmyadmin path
+	pmapath1="/etc/phpMyAdmin/config.inc.php"
+	cp $pmapath1 /root/config.inc.php.old
+}
+
+phpmyadmin_config_ubuntu(){
+	#delete old and paste new value
+	sed -i '/pmadb/d' $pmapath1
+	sed -i '/controluser/d' $pmapath1
+	sed -i '/bookmarktable/d' $pmapath1
+	sed -i '/relation/d' $pmapath1
+	sed -i '/userconfig/d' $pmapath1
+	sed -i '/table_info/d' $pmapath1
+	sed -i '/column_info/d' $pmapath1
+	sed -i '/history/d' $pmapath1
+	sed -i '/recent/d' $pmapath1
+	sed -i '/table_uiprefs/d' $pmapath1
+	sed -i '/tracking/d' $pmapath1
+	sed -i '/table_coords/d' $pmapath1
+	sed -i '/pdf_pages/d' $pmapath1
+	sed -i '/designer_coords/d' $pmapath1
+	sed -i '/controlpass/d' $pmapath1
+	sed -i '/\$i = 1; /d' $pmapath1
+	echo "\$i = 1;" >> $pmapath1
+	sed -i '/savedsearches/d' $pmapath1
+	sed -i '/navigationhiding/d' $pmapath1
+	sed -i '/users/d' $pmapath1
+	sed -i '/controlpass/d' $pmapath1
+	sed -i '/favorite/d' $pmapath1
+	sed -i '/usergroups/d' $pmapath1
+	sed -i '/central_columns/d' $pmapath1
+	sed -i '/designer_settings/d' $pmapath1
+	sed -i '/export_templates/d' $pmapath1
+	echo "\$cfg['Servers'][\$i]['favorite'] = 'pma__favorite';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['usergroups'] = 'pma__usergroups';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['central_columns'] = 'pma__central_columns';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['designer_settings'] = 'pma__designer_settings';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['export_templates'] = 'pma__export_templates';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['savedsearches'] = 'pma__savedsearches';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['navigationhiding'] = 'pma__navigationhiding';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['users'] = 'pma__users';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['usergroups'] = 'pma__usergroups';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['pmadb'] = 'phpmyadmin';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['controluser'] = 'pma';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['controlpass'] = '$PASS';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['bookmarktable'] = 'pma__bookmark';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['relation'] = 'pma__relation';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['userconfig'] = 'pma__userconfig';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['table_info'] = 'pma__table_info';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['column_info'] = 'pma__column_info';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['history'] = 'pma__history';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['recent'] = 'pma__recent';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['table_uiprefs'] = 'pma__table_uiprefs';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['tracking'] = 'pma__tracking';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['table_coords'] = 'pma__table_coords';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['pdf_pages'] = 'pma__pdf_pages';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['designer_coords'] = 'pma__designer_coords';" >> $pmapath1
+
+	sed -i '/pmadb/d' $pmapath2
+	sed -i '/controluser/d' $pmapath2
+	sed -i '/bookmarktable/d' $pmapath2
+	sed -i '/relation/d' $pmapath2
+	sed -i '/userconfig/d' $pmapath2
+	sed -i '/table_info/d' $pmapath2
+	sed -i '/column_info/d' $pmapath2
+	sed -i '/history/d' $pmapath2
+	sed -i '/recent/d' $pmapath2
+	sed -i '/table_uiprefs/d' $pmapath2
+	sed -i '/tracking/d' $pmapath2
+	sed -i '/table_coords/d' $pmapath2
+	sed -i '/pdf_pages/d' $pmapath2
+	sed -i '/designer_coords/d' $pmapath2
+	sed -i '/controlpass/d' $pmapath2
+	sed -i '/savedsearches/d' $pmapath2
+	sed -i '/navigationhiding/d' $pmapath2
+	sed -i '/users/d' $pmapath2
+	sed -i '/controlpass/d' $pmapath2
+	sed -i '/favorite/d' $pmapath2
+	sed -i '/usergroups/d' $pmapath2
+	sed -i '/central_columns/d' $pmapath2
+	sed -i '/designer_settings/d' $pmapath2
+	sed -i '/export_templates/d' $pmapath2
+	echo "\$cfg['Servers'][\$i]['favorite'] = 'pma__favorite';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['usergroups'] = 'pma__usergroups';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['central_columns'] = 'pma__central_columns';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['designer_settings'] = 'pma__designer_settings';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['export_templates'] = 'pma__export_templates';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['savedsearches'] = 'pma__savedsearches';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['navigationhiding'] = 'pma__navigationhiding';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['users'] = 'pma__users';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['usergroups'] = 'pma__usergroups';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['pmadb'] = 'phpmyadmin';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['controluser'] = 'pma';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['controlpass'] = '$PASS';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['bookmarktable'] = 'pma__bookmark';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['relation'] = 'pma__relation';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['userconfig'] = 'pma__userconfig';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['table_info'] = 'pma__table_info';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['column_info'] = 'pma__column_info';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['history'] = 'pma__history';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['recent'] = 'pma__recent';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['table_uiprefs'] = 'pma__table_uiprefs';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['tracking'] = 'pma__tracking';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['table_coords'] = 'pma__table_coords';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['pdf_pages'] = 'pma__pdf_pages';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['designer_coords'] = 'pma__designer_coords';" >> $pmapath2
+}
+phpmyadmin_config_debian(){
+	#delete old and paste new value
+	sed -i '/pmadb/d' $pmapath1
+	sed -i '/controluser/d' $pmapath1
+	sed -i '/bookmarktable/d' $pmapath1
+	sed -i '/relation/d' $pmapath1
+	sed -i '/userconfig/d' $pmapath1
+	sed -i '/table_info/d' $pmapath1
+	sed -i '/column_info/d' $pmapath1
+	sed -i '/history/d' $pmapath1
+	sed -i '/recent/d' $pmapath1
+	sed -i '/table_uiprefs/d' $pmapath1
+	sed -i '/tracking/d' $pmapath1
+	sed -i '/table_coords/d' $pmapath1
+	sed -i '/pdf_pages/d' $pmapath1
+	sed -i '/designer_coords/d' $pmapath1
+	sed -i '/controlpass/d' $pmapath1
+	sed -i '/savedsearches/d' $pmapath1
+	sed -i '/navigationhiding/d' $pmapath1
+	sed -i '/users/d' $pmapath1
+	sed -i '/controlpass/d' $pmapath1
+	sed -i '/\$i = 1; /d' $pmapath1
+	sed -i '/favorite/d' $pmapath1
+	sed -i '/usergroups/d' $pmapath1
+	sed -i '/central_columns/d' $pmapath1
+	sed -i '/designer_settings/d' $pmapath1
+	sed -i '/export_templates/d' $pmapath1
+	echo "\$cfg['Servers'][\$i]['favorite'] = 'pma__favorite';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['usergroups'] = 'pma__usergroups';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['central_columns'] = 'pma__central_columns';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['designer_settings'] = 'pma__designer_settings';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['export_templates'] = 'pma__export_templates';" >> $pmapath1
+	echo "\$i = 1;" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['savedsearches'] = 'pma__savedsearches';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['navigationhiding'] = 'pma__navigationhiding';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['users'] = 'pma__users';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['usergroups'] = 'pma__usergroups';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['pmadb'] = 'phpmyadmin';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['controluser'] = 'pma';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['controlpass'] = '$PASS';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['bookmarktable'] = 'pma__bookmark';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['relation'] = 'pma__relation';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['userconfig'] = 'pma__userconfig';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['table_info'] = 'pma__table_info';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['column_info'] = 'pma__column_info';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['history'] = 'pma__history';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['recent'] = 'pma__recent';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['table_uiprefs'] = 'pma__table_uiprefs';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['tracking'] = 'pma__tracking';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['table_coords'] = 'pma__table_coords';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['pdf_pages'] = 'pma__pdf_pages';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['designer_coords'] = 'pma__designer_coords';" >> $pmapath1
+
+	sed -i '/pmadb/d' $pmapath2
+	sed -i '/controluser/d' $pmapath2
+	sed -i '/bookmarktable/d' $pmapath2
+	sed -i '/relation/d' $pmapath2
+	sed -i '/userconfig/d' $pmapath2
+	sed -i '/table_info/d' $pmapath2
+	sed -i '/column_info/d' $pmapath2
+	sed -i '/history/d' $pmapath2
+	sed -i '/recent/d' $pmapath2
+	sed -i '/table_uiprefs/d' $pmapath2
+	sed -i '/tracking/d' $pmapath2
+	sed -i '/table_coords/d' $pmapath2
+	sed -i '/pdf_pages/d' $pmapath2
+	sed -i '/designer_coords/d' $pmapath2
+	sed -i '/controlpass/d' $pmapath2
+	sed -i '/savedsearches/d' $pmapath2
+	sed -i '/navigationhiding/d' $pmapath2
+	sed -i '/users/d' $pmapath2
+	sed -i '/controlpass/d' $pmapath2
+	sed -i '/favorite/d' $pmapath2
+	sed -i '/usergroups/d' $pmapath2
+	sed -i '/central_columns/d' $pmapath2
+	sed -i '/designer_settings/d' $pmapath2
+	sed -i '/export_templates/d' $pmapath2
+	echo "\$cfg['Servers'][\$i]['favorite'] = 'pma__favorite';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['usergroups'] = 'pma__usergroups';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['central_columns'] = 'pma__central_columns';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['designer_settings'] = 'pma__designer_settings';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['export_templates'] = 'pma__export_templates';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['savedsearches'] = 'pma__savedsearches';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['navigationhiding'] = 'pma__navigationhiding';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['users'] = 'pma__users';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['usergroups'] = 'pma__usergroups';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['pmadb'] = 'phpmyadmin';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['controluser'] = 'pma';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['controlpass'] = '$PASS';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['bookmarktable'] = 'pma__bookmark';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['relation'] = 'pma__relation';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['userconfig'] = 'pma__userconfig';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['table_info'] = 'pma__table_info';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['column_info'] = 'pma__column_info';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['history'] = 'pma__history';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['recent'] = 'pma__recent';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['table_uiprefs'] = 'pma__table_uiprefs';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['tracking'] = 'pma__tracking';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['table_coords'] = 'pma__table_coords';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['pdf_pages'] = 'pma__pdf_pages';" >> $pmapath2
+	echo "\$cfg['Servers'][\$i]['designer_coords'] = 'pma__designer_coords';" >> $pmapath2
+}
+phpmyadmin_config_centos(){
+	#delete old and paste new value
+	sed -i '/pmadb/d' $pmapath1
+	sed -i '/controluser/d' $pmapath1
+	sed -i '/bookmarktable/d' $pmapath1
+	sed -i '/relation/d' $pmapath1
+	sed -i '/userconfig/d' $pmapath1
+	sed -i '/table_info/d' $pmapath1
+	sed -i '/column_info/d' $pmapath1
+	sed -i '/history/d' $pmapath1
+	sed -i '/recent/d' $pmapath1
+	sed -i '/table_uiprefs/d' $pmapath1
+	sed -i '/tracking/d' $pmapath1
+	sed -i '/table_coords/d' $pmapath1
+	sed -i '/pdf_pages/d' $pmapath1
+	sed -i '/designer_coords/d' $pmapath1
+	sed -i '/controlpass/d' $pmapath1
+	sed -i '/savedsearches/d' $pmapath1
+	sed -i '/navigationhiding/d' $pmapath1
+	sed -i '/users/d' $pmapath1
+	sed -i '/controlpass/d' $pmapath1
+	sed -i '/favorite/d' $pmapath1
+	sed -i '/usergroups/d' $pmapath1
+	sed -i '/central_columns/d' $pmapath1
+	sed -i '/designer_settings/d' $pmapath1
+	sed -i '/export_templates/d' $pmapath1
+	echo "\$cfg['Servers'][\$i]['favorite'] = 'pma__favorite';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['usergroups'] = 'pma__usergroups';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['central_columns'] = 'pma__central_columns';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['designer_settings'] = 'pma__designer_settings';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['export_templates'] = 'pma__export_templates';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['savedsearches'] = 'pma__savedsearches';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['navigationhiding'] = 'pma__navigationhiding';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['users'] = 'pma__users';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['usergroups'] = 'pma__usergroups';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['pmadb'] = 'phpmyadmin';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['controluser'] = 'pma';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['controlpass'] = '$PASS';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['bookmarktable'] = 'pma__bookmark';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['relation'] = 'pma__relation';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['userconfig'] = 'pma__userconfig';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['table_info'] = 'pma__table_info';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['column_info'] = 'pma__column_info';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['history'] = 'pma__history';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['recent'] = 'pma__recent';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['table_uiprefs'] = 'pma__table_uiprefs';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['tracking'] = 'pma__tracking';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['table_coords'] = 'pma__table_coords';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['pdf_pages'] = 'pma__pdf_pages';" >> $pmapath1
+	echo "\$cfg['Servers'][\$i]['designer_coords'] = 'pma__designer_coords';" >> $pmapath1
+}
+
+grava_tabela_no_bando(){
+	#SOME WORK with DATABASE (table / user)
+	PMADB=phpmyadmin
+	PMAUSER=pma
+
+	#DROP USER and TABLE
+	mysql -uroot < MYSQL_PMA1
+	DROP USER '$PMAUSER'@'localhost';
+	DROP DATABASE $PMADB;
+	FLUSH PRIVILEGES;
+	MYSQL_PMA1
+
+	#CREATE PMA USER
+	mysql -uroot < MYSQL_PMA2
+	CREATE USER '$PMAUSER'@'localhost' IDENTIFIED BY '$PASS';
+	CREATE DATABASE $PMADB;
+	MYSQL_PMA2
+
+	#GRANT PMA USE SOME RIGHTS
+	mysql -uroot < MYSQL_PMA3
+	USE $PMADB;
+	GRANT USAGE ON $PMADB.* TO '$PMAUSER'@'localhost' IDENTIFIED BY '$PASS';
+	GRANT ALL PRIVILEGES ON $PMADB.* TO '$PMAUSER'@'localhost';
+	FLUSH PRIVILEGES;
+	MYSQL_PMA3
+
+	#MYSQL DB and TABLES ADDITION
+	echo "Baixando tabelas para o mysql server";
+	wget --no-check-certificate https://raw.github.com/luizjr/phpMyAdmin-Fixer-VestaCP/master/create_tables.sql;
+
+	# Check wget
+	if [ -e '/usr/bin/wget' ]; then
+		echo "Download via wget" &&
+		wget --no-check-certificate https://raw.github.com/luizjr/phpMyAdmin-Fixer-VestaCP/master/create_tables.sql;
+	else
+		echo "Erro: não foi possivel baixar via wget"
+	fi
+
+	# Check curl
+	if [ -e '/usr/bin/curl' ]; then
+		echo "Download via curl" &&
+		curl -O -k https://raw.github.com/luizjr/phpMyAdmin-Fixer-VestaCP/master/create_tables.sql;
+	else
+		echo "Erro: não foi possivel baixar via curl."
+	fi
+
+	mysql -uroot < create_tables.sql
+
+	echo "Removendo arquivos temporários";
+	rm create_tables.sql*
+	echo "pma@localhost user password = $PASS" > pma.txt
+}
+
+phpMyAdminFix(){
+	
+	am_i_root
+	boas_vindas
+	detecta_sistema
+
+	if [[ $type == "centos" ]]; then
+		#statements
+		phpmyadmin_config_centos
+
+	elif [[ $type == "debian" ]]; then
+		#statements
+		phpmyadmin_config_debian
+	else
+		phpmyadmin_config_ubuntu
+	fi
+
+	grava_tabela_no_bando
+
+	exit
+}
+
+# Rodando o script
+phpMyAdminFix
