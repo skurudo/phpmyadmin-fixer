aff2015abef24880d338e08192d26dd6d726b148
Luiz Jr <luizjrdeveloper@gmail.com>
2019-04-07 18:16:11 -0300

    Att.

--- pma.sh
+++ pma.sh
@@ -327,7 +327,7 @@ phpmyadmin_config_centos(){
 
 #### Gerando uma senha randomica e deixando como global para todas as demais funções usarem.
 #echo "Gere uma senha para o PMA (pma.txt)";
-PASS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -c 32 ; echo`
+PASS=`tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w ${1:-32} | head -c 32 ; echo`
 #echo "Senha do PMA = $PASS";
 
 phpMyAdminFix(){
